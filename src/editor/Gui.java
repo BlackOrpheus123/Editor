51d9c169b2270ac5b2ffd89758281523d3d0be8b
Leon Sackhoff <blackorpheussfdghdfnsd.dferasdf>
2016-05-25 17:57:15 +0200

    asd

--- src/editor/Gui.java
+++ src/editor/Gui.java
@@ -1,7 +1,12 @@
 package editor;
 
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.io.IOException;
+import java.util.Vector;
 
 import javax.imageio.ImageIO;
 import javax.swing.ButtonGroup;
@@ -20,8 +25,10 @@ public class Gui {
 	private String geschlechter[] = {"Männlich", "Weiblich"}; 
 	private String quests[] = {"Quest 1", "Quest 2", "Quest 3"}; 
 	private String skins_male[] = {"Bauer 1", "Bauer 2", "Bauer 3", "Ritter"};
-	private String skins_female[] = {"Frau 1", "Frau 2", "Frau 3"}; 
+	private String skins_female[] = {"Frau 1", "Frau 2", "Frau 3"};
 	
+
+	 
 
 
 	public Gui() {
@@ -64,12 +71,48 @@ public class Gui {
         ID.setBounds(100,300,150,30);
         panel.add(ID);
         
+        JLabel aussehen = new JLabel("Aussehen:");
+        aussehen.setBounds(350,30,100,100);
+        panel.add(aussehen);
+
+        JComboBox Aussehen = new JComboBox(skins_male);
+        Aussehen.setBounds(350,100,150,30);
+        Aussehen.setEnabled(true);
+        panel.add(Aussehen);
+        
         JLabel geschlecht = new JLabel("Geschlecht:");
         geschlecht.setBounds(100,330,100,100);
         panel.add(geschlecht);
         
         JComboBox Geschlecht = new JComboBox(geschlechter);
         Geschlecht.setBounds(100,400,150,30);
+        Geschlecht.addItemListener(new ItemListener() {
+        		public void itemStateChanged(ItemEvent event) {
+            if (event.getStateChange() == ItemEvent.SELECTED) {
+                Object item = event.getItem();
+                if(item == "Männlich"){
+                	for(int i=0;i<skins_male.length;i++){
+                		Aussehen.addItem(skins_male[i]);
+                	}
+                	for(int i=0;i<skins_female.length;i++){
+                		Aussehen.removeItem(skins_female[i]);
+                	}
+                	
+                	
+                	
+                }else{
+                	for(int i=0;i<skins_female.length;i++){
+                		Aussehen.addItem(skins_female[i]);
+                	}
+                	for(int i=0;i<skins_male.length;i++){
+                		Aussehen.removeItem(skins_male[i]);
+                	}
+                	
+                }
+             }
+            }
+        });
+
         panel.add(Geschlecht);
         
         JLabel type_quest = new JLabel("Quest?");
@@ -80,35 +123,38 @@ public class Gui {
         type_fight.setBounds(200,440,100,100);
         panel.add(type_fight);
         
-        JRadioButton Type_Quest = new JRadioButton();
-        Type_Quest.setBounds(110, 510, 20, 20);
-        panel.add(Type_Quest);
-  
-        JRadioButton Type_Fight = new JRadioButton();
-        Type_Fight.setBounds(210, 510, 20, 20);
-        panel.add(Type_Fight);
-        
-        ButtonGroup group = new ButtonGroup();	
-        group.add(Type_Quest);
-        group.add(Type_Fight);
-        
         JLabel choose_quest = new JLabel("Quest wählen:");
         choose_quest.setBounds(100,530,100,100);
         panel.add(choose_quest);
         
         JComboBox Choose_quest = new JComboBox(quests);
         Choose_quest.setBounds(100,600,150,30);
-        Choose_quest.setEnabled(true);
+        Choose_quest.setEnabled(false);
         panel.add(Choose_quest);
         
-        JLabel aussehen = new JLabel("Aussehen:");
-        aussehen.setBounds(350,30,100,100);
-        panel.add(aussehen);
-
-        JComboBox Aussehen = new JComboBox(skins_male);
-        Aussehen.setBounds(350,100,150,30);
-        Aussehen.setEnabled(true);
-        panel.add(Aussehen);
+        JRadioButton Type_Quest = new JRadioButton();
+        Type_Quest.setBounds(110, 510, 20, 20);
+        Type_Quest.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            Choose_quest.setEnabled(true);
+            }
+        });
+        panel.add(Type_Quest);
+  
+        JRadioButton Type_Fight = new JRadioButton();
+        Type_Fight.setBounds(210, 510, 20, 20);
+        Type_Fight.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            Choose_quest.setEnabled(false);
+            }
+        });
+        panel.add(Type_Fight);
+        
+        ButtonGroup group = new ButtonGroup();	
+        group.add(Type_Quest);
+        group.add(Type_Fight);
         
         JLabel health = new JLabel("Gesundheit:");
         health.setBounds(350,130,100,100);
@@ -121,13 +167,16 @@ public class Gui {
         frame.add(panel);
         frame.setVisible(true);
         
-
+        //Def, Damage, Iniziative, XP, Lost XP,
+        //Task, Ziel, Anzahl, Belohnung, Anzahl Belohnung, Xp
         panel.add(mainlabel);
         
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+
+
 	
 
 
